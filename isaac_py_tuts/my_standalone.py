514c401ac8aa20619fef856f53e782e43c5d3762
vaibhavparekh9 <153970846+vaibhavparekh9@users.noreply.github.com>
2025-06-09 03:17:30 -0400

    Add files via upload

--- isaac_py_tuts/my_standalone.py
+++ isaac_py_tuts/my_standalone.py
@@ -1,4 +1,8 @@
 from isaacsim import SimulationApp
+"""
+NOTE: Any Omniverse level imports must occur after the class is instantiated.
+Because APIs are provided by the extension/runtime plugin system, it must be loaded before they will be available to import.
+"""
 
 # Start the application
 simulation_app = SimulationApp({"headless": False})
@@ -16,4 +20,7 @@ while simulation_app.is_running():
 
 # Close when update is stopped (manually closed)
 simulation_app.close()
+
+
+# this code can be found at: https://docs.isaacsim.omniverse.nvidia.com/4.5.0/python_scripting/manual_standalone_python.html
 
