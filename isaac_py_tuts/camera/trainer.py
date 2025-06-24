e3b5e28d874989493a7522de6e5014015d34f654
vaibhavparekh9 <153970846+vaibhavparekh9@users.noreply.github.com>
2025-06-23 21:30:12 -0400

    Update trainer.py

--- isaac_py_tuts/camera/trainer.py
+++ isaac_py_tuts/camera/trainer.py
@@ -4,7 +4,3 @@ model = YOLO("yolov8n.pt")
 
 results = model.train(data="/home/vsparekh/isaac_py_tuts/src/camera/detect/geo shapes detection.v1i.yolov11/data.yaml", 
                       epochs=100, imgsz=640, batch=6, device='cuda')
-
-# # (Optional) Evaluate on your validation set
-# metrics = model.val(data="data.yaml")
-# print(metrics)pip install ultralytics
