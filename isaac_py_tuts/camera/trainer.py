9b517a76c876cab3e2583dbe0adc28d695fc8b11
vaibhavparekh9 <vsparekh2000@gmail.com>
2025-06-30 00:38:11 -0400

    updated with Random Pose Gen

--- isaac_py_tuts/camera/trainer.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-results = model.train(data="/home/vsparekh/isaac_py_tuts/src/camera/detect/geo shapes detection.v1i.yolov11/data.yaml", 
-                      epochs=100, imgsz=640, batch=6, device='cuda')
